f46d0129957133cef1ad6c2e339da6f7068a458f
Hunc Tasci <hunc.tasci@gmail.com>
2023-04-12 18:41:41 +0300

    fixed functionality

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const ratings = document.querySelectorAll("button");
+const ratings = document.querySelectorAll("#rate");
 const ratingDiv = document.querySelector("#rating-modal");
 const thankYouDiv = document.querySelector("#thank-you-modal");
 const submitButton = document.querySelector("#submit-button");
-const actualRating = document.getElementById("score");
+const span = document.getElementById("score");
+
+let number = "";
+ratings.forEach((rating) => {
+  rating.addEventListener("click", (e) => {
+    number = e.target.innerText;
+    span.innerText = number;
+  });
+});
 
 submitButton.addEventListener("click", () => {
   ratingDiv.style.display = "none";
   thankYouDiv.classList.remove("hidden");
-
-  ratings.forEach((rating) => {
-    rating.addEventListener("click", () => {
-      const score = rating.value;
-      const scoreNum = Number(score);
-
-      actualRating.innerText = scoreNum;
-    });
-  });
 });
